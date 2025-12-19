47b65b2740b184aa0bc6e25b7d799e45a53612d6
ansa-23 <ansa.hameed23@gmail.com>
2025-12-19 14:45:56 +0500

    folder

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("helo")
+console.log("hello world");
+alert("Hello world");
+ let num = +prompt("Enter any number:");
+
+    let absValue = Math.abs(num);
+
+    document.write("Absolute value: " + absValue);
